569998b5732161381dc98c9c2c94ae123098a813
Gustavo <gugul5@hotmail.com>
2022-04-13 14:10:29 -0300

    deploy

---
+++ src/components/postagens/cadastroPost/CadastroPost.tsx
@@ -0,0 +1,213 @@
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import { Container, Typography, TextField, Button, Select, InputLabel, MenuItem, FormControl, FormHelperText, Grid } from "@material-ui/core"
+import './CadastroPost.css';
+import { Link, useHistory, useParams } from 'react-router-dom';
+import Tema from '../../../models/Tema';
+import useLocalStorage from 'react-use-localstorage';
+import Postagem from '../../../models/Postagem';
+import { busca, buscaId, post, put } from '../../../services/Service';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { toast } from 'react-toastify';
+
+function CadastroPost() {
+    let history = useHistory();
+    const { id } = useParams<{ id: string }>();
+    const [temas, setTemas] = useState<Tema[]>([])
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    useEffect(() => {
+        if (token == "") {
+            toast.info('Usuário deslogado', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+            history.push("/login")
+
+        }
+    }, [token])
+
+    const [tema, setTema] = useState<Tema>(
+        {
+            id: 0,
+            descricao: ''
+        })
+    const [postagem, setPostagem] = useState<Postagem>({
+        id: 0,
+        titulo: '',
+        texto: '',
+        tema: null
+    })
+
+    useEffect(() => {
+        setPostagem({
+            ...postagem,
+            tema: tema
+        })
+    }, [tema])
+
+    useEffect(() => {
+        getTemas()
+        if (id !== undefined) {
+            findByIdPostagem(id)
+        }
+    }, [id])
+
+    async function getTemas() {
+        await busca("/temas", setTemas, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    async function findByIdPostagem(id: string) {
+        await buscaId(`postagens/${id}`, setPostagem, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function updatedPostagem(e: ChangeEvent<HTMLInputElement>) {
+
+        setPostagem({
+            ...postagem,
+            [e.target.name]: e.target.value,
+            tema: tema
+        })
+
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        if (id !== undefined) {
+
+            try {
+                await put(`/postagens`, postagem, setPostagem, {
+                    headers: {
+                        'Authorization': token
+                    }
+                })
+                toast.success('Postagem atualizada com sucesso', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    theme: "colored",
+                    progress: undefined,
+                });
+                back()
+            }
+
+            catch (error) {
+                toast.error('Dados da postagem inconsistentes.', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    theme: "colored",
+                    progress: undefined,
+                });
+
+                setPostagem({ ...postagem, titulo: "", texto: "" } ) 
+                
+            }
+
+        } else {
+            try {
+                await post(`/postagens`, postagem, setPostagem, {
+                    headers: {
+                        'Authorization': token
+                    }
+                })
+                toast.success('Postagem cadastrada com sucesso', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    theme: "colored",
+                    progress: undefined,
+                });
+
+                back()
+
+            } catch (error) {
+                toast.error('Dados da postagem inconsistentes.', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    theme: "colored",
+                    progress: undefined,
+                });
+
+                setPostagem({ ...postagem, titulo: "", texto: "" }) 
+            }
+
+        }
+
+    }
+
+    function back() {
+        history.push('/posts')
+    }
+
+    return (
+        <Container maxWidth="sm" className="topo testando">
+            <form onSubmit={onSubmit}>
+                <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro postagem</Typography>
+                <TextField value={postagem.titulo} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPostagem(e)} id="titulo" label="Título" variant="outlined" name="titulo" margin="normal" placeholder="Digite o título da sua publicação" fullWidth />
+                <TextField value={postagem.texto} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPostagem(e)} id="texto" label="Texto" name="texto" variant="outlined" margin="normal" placeholder="Mínimo: 10. Máximo: 1000" fullWidth />
+
+                <FormControl >
+                    <InputLabel id="demo-simple-select-helper-label">Tema </InputLabel>
+                    <Select
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        onChange={(e) => buscaId(`/temas/${e.target.value}`, setTema, {
+                            headers: {
+                                'Authorization': token
+                            }
+                        })}>
+                        {
+                            temas.map(tema => (
+                                <MenuItem value={tema.id}>{tema.descricao}</MenuItem>
+                            ))
+                        }
+                    </Select>
+                    <FormHelperText>Escolha um tema para a postagem</FormHelperText>
+
+                </FormControl>
+                <Grid container alignItems='center' >
+                    <Button type="submit" variant="contained" color="primary" className='margin botaoFINALZIAR'>
+                        Finalizar
+                    </Button>
+                    <Link to='/posts' className='text-decorator-none'>
+                        <Button variant='contained' color='secondary' className='btnCancelar'>
+                            Cancelar
+                        </Button>
+                    </Link>
+                </Grid>
+            </form>
+        </Container>
+    )
+}
+export default CadastroPost;
---
+++ src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -0,0 +1,136 @@
+import React, {useState, useEffect, ChangeEvent} from 'react';
+import {useHistory} from 'react-router-dom';
+import User from '../../models/User';
+import { cadastroUsuario } from '../../services/Service';
+import './CadastroUsuario.css';
+import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+function CadastroUsuario() {
+
+    let history = useHistory();
+    const [confirmarSenha,setConfirmarSenha] = useState<String>("")
+    const [user, setUser] = useState<User>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: ''
+        })
+
+    const [userResult, setUserResult] = useState<User>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: ''
+        })
+
+    useEffect(() => {
+        if (userResult.id != 0) {
+            history.push("/login")
+        }
+    }, [userResult])
+
+
+    function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>){
+        setConfirmarSenha(e.target.value)
+    }
+
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+
+    }
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        // Estrutura Condicional que verifica se as senhas batem e se a Senha tem mais de 8 caracteres
+        if (confirmarSenha === user.senha && user.senha.length >= 8) {
+
+            //Tenta executar o cadastro
+            try {
+                await cadastroUsuario(`/usuarios/cadastrar`, user, setUserResult)
+                toast.success('Usuário cadastrado com sucesso', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    theme: "colored",
+                    progress: undefined,
+                });
+
+            //Se houver erro, pegue o Erro e retorna uma msg
+            } catch (error) {
+                console.log(`Error: ${error}`)
+                
+                //Pode modificar a msg de acordo com o erro 
+                toast.error('Dados inconsistentes. Tente novamente', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    theme: "colored",
+                    progress: undefined,
+                });
+            }
+
+        } else {
+            toast.error('Dados inconsistentes. Tente novamente', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+
+            setUser({ ...user, senha: "" }) // Reinicia o campo de Senha
+            setConfirmarSenha("")           // Reinicia o campo de Confirmar Senha
+        }
+    }
+
+    return (
+        <Grid container direction='row' justifyContent='center' alignItems='center' >
+            <Grid item xs={6} className='imagem2'></Grid>
+            <Grid item xs={6} alignItems='center'>
+                <Box paddingX={10}>
+                    <form onSubmit={onSubmit}>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos2'>
+                            Cadastre-se</Typography>
+                        <TextField value={user.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='nome' label='Nome' variant='outlined' name='nome' margin='normal' placeholder='Digite seu nome completo' required fullWidth />
+                        <TextField value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} type = 'email' id='usuario' label='Usuário' variant='outlined' name='usuario' margin='normal' placeholder='Seu email funcionará como um usuário válido. Ex: exemplo@exemplo.com' required fullWidth />
+                        <TextField value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' placeholder='Senha com no mínimo 8 caracteres' required fullWidth />
+                        <TextField value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id='confirmarSenha' label='Confirmar senha' variant='outlined' name='confirmarSenha' margin='normal' type='password' placeholder='Digite sua seha novamente' required fullWidth />
+                        <Box marginTop={2} textAlign='center'>
+                            <Link to='/login' className='text-decorator-none'>
+                                <Button variant='contained' color='secondary' className='btnCancelar'>
+                                    Cancelar
+                                </Button>
+                            </Link>
+                            <Button type ='submit' variant='contained' color='primary' className='btnCadastrar'>
+                                Cadastrar
+                            </Button>
+                        </Box>
+                    </form>
+                </Box>
+
+            </Grid>
+
+
+        </Grid>
+    );
+}
+
+export default CadastroUsuario;
